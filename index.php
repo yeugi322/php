b9cbdc877d699b37dc630b643222c93cd37888c1
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-23 17:08:20 +0800

    experimented with radio buttons

--- index.php
+++ index.php
@@ -7,28 +7,36 @@
 </head>
 <body>
     <form action="index.php" method="post">
-        <label>username:</label>
-        <input type="text" name="username"><br>
-        <label>password:</label>
-        <input type="password" name="password"><br>
-        <input type="submit" name="login" value="Submit">
+        <input type="radio" name="credit_card" value="Visa">Visa<br>
+        <input type="radio" name="credit_card" value="Mastercard">Mastercard<br>
+        <input type="radio" name="credit_card" value="AmericanExpress">American Express<br>
+        <input type="submit" name="confirm" value="confirm">
     </form>
 </body>
 </html>
 <?php
+    if(isset($_POST["confirm"])){
+        $credit_card = null;
+        if(isset($_POST["credit_card"])){
+            $credit_card = $_POST["credit_card"];
+            
+        } 
 
-    foreach($_POST as $key => $value){
-        echo"{$key} = {$value} <br>";
+        switch($credit_card){
+            case "Visa":
+                echo"You selected Visa";
+                break;
+            case "Mastercard":
+                echo"You selected Mastercard";
+                break;
+            case "AmericanExpress":
+                echo"You selected American Express";
+                break;  
+            default:
+                echo"Please make a selection";
+        }
+        
+        
     }
-   if(isset($_POST["login"])){
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    if(empty($username)){
-        echo"username is missing! <br>";
-    } else if(empty($password)){
-        echo"password is missing! <br>";
-    } else {
-        echo"Hello, {$username}!";
-    }
-   }
+    
 ?>
