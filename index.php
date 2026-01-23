6d5c2d48a6017a6ba1a2d565552c4db65e96ceed
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-23 14:27:41 +0800

    experimented with for loops

--- index.php
+++ index.php
@@ -7,25 +7,16 @@
 </head>
 <body>
     <form action="index.php" method="post">
-        <label for="">radius:</label><br>
-        <input type="text" name="radius">
-        <input type="submit" value="Calculate">
+        <label for="">Enter a number to count to:</label><br>
+        <input type="text" name="counter">
+        <input type="submit" value="Start">
     </form>
 </body>
 </html>
 <?php
-   $radius = $_POST["radius"];
-   $circumference = null;
-    $area = null;
-    $volume = null;
+   $counter =$_POST["counter"];
 
-   $circumference = 2 * pi() * $radius;
-   $area = pi() * pow($radius, 2);
-   $volume = 4/3 * pi() * pow($radius, 3);
-   $area = round($area, 2);
-    $circumference = round($circumference, 2);
-    $volume = round($volume, 2);
-   echo"Circumference = {$circumference}cm <br>";
-   echo"Area = {$area}cm^2 <br>";
-   echo"Volume = {$volume}cm^3 <br>";
+   for($i = 0; $i <= $counter; $i++){
+    echo $i . "<br>";
+   }
 ?>
