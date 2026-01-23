12efc9c1cf37750fd6a69d16205ab03df3de16c8
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-23 16:54:56 +0800

    experimented with isset and empty functions

--- index.php
+++ index.php
@@ -1,13 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label>username:</label>
+        <input type="text" name="username"><br>
+        <label>password:</label>
+        <input type="password" name="password"><br>
+        <input type="submit" name="login" value="Submit">
+    </form>
+</body>
+</html>
 <?php
-   $foods = array("apple","orange","banana","coconut");
-   //$foods[0] = "pineapple";
-   //array_push($foods, "pineapple");
-   //array_pop($foods);
-   //array_shift($foods);
-    //$foods = array_reverse($foods);
-    echo count($foods);
 
-   foreach($foods as $food){
-    echo $food . "<br>";
+    foreach($_POST as $key => $value){
+        echo"{$key} = {$value} <br>";
+    }
+   if(isset($_POST["login"])){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    if(empty($username)){
+        echo"username is missing! <br>";
+    } else if(empty($password)){
+        echo"password is missing! <br>";
+    } else {
+        echo"Hello, {$username}!";
+    }
    }
 ?>
