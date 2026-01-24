28494bd135c9a641bc6f5a8dd598fa818bacd7b6
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-24 14:59:09 +0800

    created login page with database connection that hashes passwords and checks username is taken or not

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+    include("database.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,22 +9,39 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php" method="post">
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <h2>Welcome to Fakebook</h2>
         username:<br>
         <input type="text" name="username"><br>
         password:<br>
         <input type="password" name="password"><br>
-        <input type="submit" name="login" value="login">
+        <input type="submit" name="submit" value="register">
     </form>
 </body>
 </html>
 <?php 
-    if(isset($_POST["login"])){
-        if(!empty($_POST["username"]) && !empty($_POST["password"])) {
-            $_SESSION["username"] = $_POST["username"];
-            $_SESSION["password"] = $_POST["password"];
 
-            header("Location: home.php");
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+        $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if(empty($username)){
+            echo"Please enter username!<br>";
+        } elseif(empty($password)){
+            echo"Please enter password!<br>";
+        } else{
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (user, password)
+                    VALUES ('$username', '$hash')";
+            try{
+                mysqli_query($conn, $sql);
+                echo"You are now registered!";
+            } catch(mysqli_sql_exception){
+                echo"Username is taken!";
+            }
+            
         }
     }
+
+    mysqli_close($conn);
 ?>
