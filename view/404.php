a1b528609dca9ac77a2f09492348304abcac6ab8
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-29 16:44:47 +0800

    changed the old path to new path

--- view/404.php
+++ view/404.php
@@ -1,5 +1,5 @@
-    <?php require 'partials/head.php'; ?>
-    <?php require 'partials/nav.php'; ?>
+<?php require base_path('view/partials/head.php'); ?>
+<?php require base_path('view/partials/nav.php'); ?>
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 <p class="text-sm font-medium text-white">404 - Page Not Found</p>
